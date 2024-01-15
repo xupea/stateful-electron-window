b5e282a2bb6fe7e6777e775df563c5dd535dc91e
xupea <xupea@192.168.0.103>
2024-01-15 23:22:52 +0800

    test: add unit test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,4 +1,77 @@
-test("adds two numbers correctly", () => {
-  const result = 3;
-  expect(result).toBe(3);
+import path from "path";
+import { readFileSync } from "jsonfile";
+
+import { StatefulBrowserWindow } from "../src/index";
+
+jest.mock("jsonfile");
+
+// Mocking BrowserWindow
+jest.mock("electron", () => {
+  const originalElectron = jest.requireActual("electron");
+  return {
+    ...originalElectron,
+    app: {
+      getPath: jest.fn(() => "/path/to/user/data"),
+    },
+  };
+});
+
+beforeEach(() => {
+  (readFileSync as any).mockClear();
+});
+
+test("tries to read state file from the default location", () => {
+  (readFileSync as any).mockImplementation(() => {
+    return jest.fn();
+  });
+
+  new StatefulBrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  expect(readFileSync).toHaveBeenCalled();
+  expect(readFileSync).toHaveBeenCalledWith(
+    "/path/to/user/data/window-state.json",
+  );
+});
+
+test("tries to read state file from the configured source", () => {
+  (readFileSync as any).mockImplementation(() => {
+    return jest.fn();
+  });
+
+  new StatefulBrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    configFilePath: "/path/custom/data",
+    configFileName: "state.json",
+  });
+
+  expect(readFileSync).toHaveBeenCalled();
+  expect(readFileSync).toHaveBeenCalledWith("/path/custom/data/state.json");
+});
+
+test("considers the state invalid if without bounds", () => {
+  (readFileSync as any).mockImplementation(() => {
+    return () => ({ width: 100 });
+  });
+
+  const win = new StatefulBrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    configFilePath: "/path/custom/data",
+    configFileName: "state.json",
+  });
+
+  expect(win.getBounds().width).not.toBe(100);
 });
